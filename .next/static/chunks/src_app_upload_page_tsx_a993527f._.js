e91573c09c3ae76de273aa2306e048c8c7fcb1e7
amirxo1313 <amirsharafdeveloper@gmail.com>
2025-07-04 18:25:08 +0330

    Add .next/static/chunks/src_app_upload_page_tsx_a993527f._.js 1751640906691

---
+++ .next/static/chunks/src_app_upload_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_0a38692d._.js",
+  "static/chunks/src_92a49dd6._.js"
+],
+    source: "dynamic"
+});
